868a4f3589748da818348a8ddbf1a691f71337b2
lingyun.xie <13840824371@163.com>
2023-09-05 10:09:30 +0800

    init black-horse-sep web application

---
+++ src/main/java/com/example/blackhorsesep/BlackHorseSepApplication.java
@@ -0,0 +1,16 @@
+package com.example.blackhorsesep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linyun.xie
+ */
+@SpringBootApplication
+public class BlackHorseSepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackHorseSepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blackhorsesep/BlackHorseSepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackhorsesep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackHorseSepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
