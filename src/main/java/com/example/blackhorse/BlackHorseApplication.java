d4668a20bb4f16f5e43fea8161da4f7dacc7f1ab
lingyun.xie <13840824371@163.com>
2023-11-12 14:17:58 +0800

    init application

---
+++ src/main/java/com/example/blackhorse/BlackHorseApplication.java
@@ -0,0 +1,16 @@
+package com.example.blackhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linyun.xie
+ */
+@SpringBootApplication
+public class BlackHorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackHorseApplication.class, args);
+	}
+
+}
